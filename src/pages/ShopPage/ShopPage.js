a49a6e7ba709af04cd2b39968ced8c004e825dee
unknown <gracjanludyga@gmail.com>
2020-09-16 15:55:53 +0200

    use iconsItem component in shopPage with router switch

--- src/pages/ShopPage/ShopPage.js
+++ src/pages/ShopPage/ShopPage.js
@@ -1,19 +1,60 @@
 import React from "react";
 
-import { Menu, BottomBarWithIcons } from "components";
+import { Switch, Route } from "react-router-dom";
+
+import { Menu, BottomBarWithIcons, PageInfo, IconsItem } from "components";
 
 import { MainPageWrapper } from "assets/StyledComponents/MainPageWrapper.css";
 
 import { IconElementsShopItemsPage } from "assets";
 
+import {
+  groceries_coloredImg,
+  products_coloredImg,
+  dishes_coloredImg,
+  lists_coloredImg,
+} from "pictures/ParentCategoryIcons";
+
 function ShopPage() {
   return (
     <>
-      <Menu
-        iconElementsList={IconElementsShopItemsPage}
-        activeIcon={"shop"}
-      />
-      <MainPageWrapper>ShopPage</MainPageWrapper>
+      <Menu iconElementsList={IconElementsShopItemsPage} activeIcon={"shop"} />
+      <PageInfo description={"Shop"} />
+      <Switch>
+        <Route exact path="/shop">
+          <MainPageWrapper>
+            <IconsItem
+              image={groceries_coloredImg}
+              text="groceries"
+              to="/shop/groceries"
+            />
+            <IconsItem
+              image={products_coloredImg}
+              text="products"
+              to="/shop/groceries"
+            />
+            <IconsItem
+              image={dishes_coloredImg}
+              text="dishes"
+              to="/shop/dishes"
+            />
+            <IconsItem image={lists_coloredImg} text="lists" to="/shop/lists" />
+          </MainPageWrapper>
+        </Route>
+        <Route exact path="/shop/groceries">
+          <MainPageWrapper>shop/groceries</MainPageWrapper>
+        </Route>
+        <Route exact path="/shop/products">
+          <MainPageWrapper>shop/products</MainPageWrapper>
+        </Route>
+        <Route exact path="/shop/dishes">
+          <MainPageWrapper>shop/dishes</MainPageWrapper>
+        </Route>
+        <Route exact path="/shop/lists">
+          <MainPageWrapper>shop/lists</MainPageWrapper>
+        </Route>
+      </Switch>
+
       <BottomBarWithIcons />
     </>
   );
